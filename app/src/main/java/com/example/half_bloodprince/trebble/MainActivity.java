9bf99ea6a4e7902ac22281d5c86e61ceb031d36c
NarnePranay <pn148@snu.edu.in>
2018-08-25 04:37:37 +0530

    small changes

--- app/src/main/java/com/example/half_bloodprince/trebble/MainActivity.java
+++ app/src/main/java/com/example/half_bloodprince/trebble/MainActivity.java
@@ -5,7 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -27,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
+
 
 class RetrieveFeedTask extends AsyncTask<String, Void, Void> {
 
