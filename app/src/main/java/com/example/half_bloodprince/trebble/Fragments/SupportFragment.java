71f2d8a038e72807018ba160637c59ea2fc20e5b
ks692 <ks692@snu.edu.in>
2018-08-27 02:10:23 +0530

    faq complete push

--- app/src/main/java/com/example/half_bloodprince/trebble/Fragments/SupportFragment.java
+++ app/src/main/java/com/example/half_bloodprince/trebble/Fragments/SupportFragment.java
@@ -1,12 +1,15 @@
 package com.example.half_bloodprince.trebble.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.half_bloodprince.trebble.FAQActivity;
 import com.example.half_bloodprince.trebble.R;
 
 /**
@@ -21,10 +24,17 @@ public class SupportFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_support, container, false);
+        View view=inflater.inflate(R.layout.fragment_support,container,false);
+        Button b1=(Button)view.findViewById(R.id.faqs);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), FAQActivity.class));
+            }
+        });
+        return view;
     }
 
 }
